8fef99111831400ced5a51d5c04db316d55b1874
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-10-07 15:10:10 +0200

    Update

---
+++ HackerRank/Flow/ElectronicsShop.cs
@@ -0,0 +1,25 @@
+namespace HackerRank.Flow;
+
+public class ElectronicsShop
+{
+    static int getMoneySpent(int[] keyboards, int[] drives, int b)
+    {
+        var closestToBudget = 0;
+
+        foreach (var keyboard in keyboards)
+        {
+            foreach (var drive in drives)
+            {
+                var cost = keyboard + drive;
+
+                if (cost > closestToBudget && cost <= b)
+                {
+                    closestToBudget = cost;
+                }
+            }
+        }
+
+        return closestToBudget > 0 ? closestToBudget : -1;
+
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,6 @@
+﻿using HackerRank;
+using HackerRank.Flow;
+
+var valleys = CountingValleys.countingValleys(CountingValleys.Path.Length, CountingValleys.Path);
+
+Console.WriteLine(valleys);
